76333e81b4127af2d93e0318c1b094d791fd5d69
isaac-silv <isaacaquino.dv@gmail.com>
2022-12-02 14:51:52 -0300

    Adicionando avatar na tabela de alunos

--- src/pages/alunos/Alunos.tsx
+++ src/pages/alunos/Alunos.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TablePagination, TableRow } from '@mui/material';
+import { Avatar, Box, Grid, Icon, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TablePagination, TableRow } from '@mui/material';
 import { AppToolbar } from '../../shared/components';
 import { LayoutBase } from '../../shared/layouts';
 
@@ -11,38 +11,61 @@ export const Alunos = () => {
         <AppToolbar />
       )}
     >
-      <Grid container >
-        <TableContainer component={Paper}>
-          <Table >
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  Aluno(a)
-                </TableCell>
-                <TableCell align='right'>
-                  Turma
-                </TableCell>
-                <TableCell align='right'>
-                  Turno
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <Box display='flex' alignItems='center' gap={2}>
-                    <Avatar />
-                    Isaac Silva
-                  </Box>
-                </TableCell>
-                <TableCell align='right'>
-                  2º ano
-                </TableCell>
-                <TableCell align='right'>
-                  Tarde
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
+      <Grid container spacing={2}>
+        <Grid item container xs={8}>
+          <TableContainer component={Paper}>
+            <Table >
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    Aluno(a)
+                  </TableCell>
+                  <TableCell align='right'>
+                    Contato
+                  </TableCell>
+                  <TableCell align='right'>
+                    Turma
+                  </TableCell>
+                  <TableCell align='right'>
+                    Turno
+                  </TableCell>
+                  <TableCell align='right'>
+                    Editar
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <Box display='flex' alignItems='center' gap={2}>
+                      <Avatar src='https://www.svgrepo.com/show/8137/avatar.svg' />
+                      Isaac Silva
+                    </Box>
+                  </TableCell>
+                  <TableCell align='right'>
+                    (99) 90000-0000
+                  </TableCell>
+                  <TableCell align='right'>
+                    2º ano
+                  </TableCell>
+                  <TableCell align='right'>
+                    Tarde
+                  </TableCell>
+                  <TableCell align='right'>
+                    <IconButton>
+                      <Icon>
+                        manage_accounts
+                      </Icon>
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper>
+            Ranking
+          </Paper>
+        </Grid>
       </Grid>
     </LayoutBase>
   );
